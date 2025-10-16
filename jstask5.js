e27c85cfcb2a548ed84b0fd5300d308760c944ec
Minahil Fatima <meerubawan471@gmail.com>
2025-10-16 17:20:16 +0500

    Update jstask5.js

--- jstask5.js
+++ jstask5.js
@@ -1,9 +1,11 @@
+//Task 1
 const changeColor = () => {
   let paras = document.querySelectorAll(".myPara"); 
   paras[0].style.color = "blue";
   paras[1].style.color = "green"; 
   paras[2].style.color = "yellow";
 }
+//Task 2
 const changeBorders = () => {
   let images = document.querySelectorAll(".myImage");
   if (images.length >= 4) {
@@ -15,3 +17,4 @@ const changeBorders = () => {
     console.error("Not enough elements with class 'myImage' found.");
   }
 }
+
